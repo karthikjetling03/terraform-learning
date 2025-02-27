9d7733001a9a1a8d34ae0b8043739bb2f0507c29
Karthik Jetling <karthikjetling@gmail.com>
2025-02-27 23:17:56 +0530

    added the type for the defined variables

--- variable.tf
+++ variable.tf
@@ -1,27 +1,27 @@
 variable "aws_region" {
-
+  type = string
 }
 
 variable "vpc_cidr_block" {
-
+  type = string
 }
 
 variable "subnet_cidr_block" {
-
+  type = string
 }
 
 variable "subnet_az" {
-
+  type = string
 }
 
 variable "internet_cidr_block" {
-
+  type = string
 }
 
 variable "ec2_instance_type" {
-
+  type = string
 }
 
 variable "ec2_key_name" {
-
+  type = string
 }
